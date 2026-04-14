178650de6dafddb2b144da0fe9649fd48479a583
Marcus <marcus.paixao02@gmail.com>
2026-04-14 16:43:27 -0300

    Primerio commit da atividade

---
+++ index.js
@@ -0,0 +1,65 @@
+/*
+//Usando IF | ELSE IF | ELSE
+function Partidas (vitorias, derrotas){
+    return vitorias - derrotas
+}
+let SaldoPartidas = Partidas (125,24)
+
+if (SaldoPartidas <=10) {
+    nivel = "Ferro"
+    console.log("O Herói tem de saldo ", SaldoPartidas,"e está no nivel", nivel)
+}
+else if (SaldoPartidas >=11 && SaldoPartidas <=20) {
+     nivel = "Bronze"
+    console.log("O Herói tem de saldo ", SaldoPartidas,"e está no nivel", nivel)
+} 
+else if (SaldoPartidas >=21 && SaldoPartidas <=50) {
+     nivel = "Prata"
+    console.log("O Herói tem de saldo ", SaldoPartidas,"e está no nivel", nivel)
+}
+else if (SaldoPartidas >=51 && SaldoPartidas <=80) {
+     nivel = "Ouro"
+    console.log("O Herói tem de saldo ", SaldoPartidas,"e está no nivel", nivel)
+} 
+else if (SaldoPartidas >=81 && SaldoPartidas <=90) {
+     nivel = "Diamante"
+    console.log("O Herói tem de saldo ", SaldoPartidas,"e está no nivel", nivel)
+}
+else if (SaldoPartidas >=91 && SaldoPartidas <=100) {
+     nivel = "Lendario"
+    console.log("O Herói tem de saldo ", SaldoPartidas,"e está no nivel", nivel)
+} 
+else {
+     nivel = "Imortal"
+    console.log("O Herói tem de saldo ", SaldoPartidas,"e está no nivel", nivel)
+};
+
+//Usando SWICH CASE
+function Partidas (vitorias, derrotas){
+    return vitorias - derrotas
+}
+let SaldoPartidas = Partidas (50,0)
+
+switch (true){
+    case SaldoPartidas <=10:
+        console.log("O Herói tem de saldo", SaldoPartidas,"e está no nivel Ferro")
+        break;
+    case SaldoPartidas <=20:
+        console.log("O Herói tem de saldo", SaldoPartidas,"e está no nivel Bronze")
+        break;
+    case SaldoPartidas <=50:
+        console.log("O Herói tem de saldo", SaldoPartidas,"e está no nivel Prata")
+        break;
+    case SaldoPartidas <=80:
+        console.log("O Herói tem de saldo", SaldoPartidas,"e está no nivel Ouro")
+        break;
+    case SaldoPartidas <=90:
+        console.log("O Herói tem de saldo", SaldoPartidas,"e está no nivel Diamante")
+        break;
+    case SaldoPartidas <=100:
+        console.log("O Herói tem de saldo", SaldoPartidas,"e está no nivel Lendario")
+        break;
+    default:
+        console.log("O Herói tem de saldo", SaldoPartidas,"e está no nivel Imortal")
+}
+*/
